5ab1c9471cff25b8d17338b978d570ffd9f0d398
psison <sisonpatrick29@yahoo.com>
2022-09-15 15:50:53 -0500

    implement create kitty functionality

--- controllers/kitties.js
+++ controllers/kitties.js
@@ -1,7 +1,19 @@
 import { Kitty } from "../models/kitty.js"
 
 function create(req, res) {
-
+  req.body.owner = req.user.profile
+  Kitty.create(req.body)
+  .then(kitty => {
+    Kitty.findById(kitty._id)
+    .populate('owner')
+    .then(populatedKitty => {
+      res.json(populatedKitty)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json({err: err.errmsg})
+  })
 }
 
 export {
